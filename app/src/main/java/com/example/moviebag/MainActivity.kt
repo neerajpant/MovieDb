788be8febfc26efcd95168eeea3bb9d76d3f6a48
Neeraj Pant <Neeraj.pant@resideo.com>
2021-03-06 10:34:48 +0530

    first commit

---
+++ app/src/main/java/com/example/moviebag/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.moviebag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var frameLayout: FrameLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun initaliseFragment()
+    {
+
+    }
+}
